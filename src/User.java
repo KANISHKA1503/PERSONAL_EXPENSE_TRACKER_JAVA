53dd4d6eb253d01958ef27b26173be2bef3a2df1
KANISHKA1503 <kanishka.s2024cse@sece.ac.in>
2025-09-20 20:20:07 +0530

    Added Getters and setters

--- src/User.java
+++ src/User.java
@@ -1,30 +1,54 @@
 public class User {
-    private int id;
-    private String username;
-    private String password;
-    private String email;
-    
-    public User() {}
-    
-    public User(String username, String password, String email) {
-        this.username = username;
-        this.password = password;
-        this.email = email;
+    int id;
+    String uname;
+    String pass;
+    String email;
+
+    User() {
     }
-    
-    public User(int id, String username, String password, String email) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.email = email;
+
+    User(String u, String p, String e) {
+        uname = u;
+        pass = p;
+        email = e;
     }
-    
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+
+    User(int i, String u, String p, String e) {
+        id = i;
+        uname = u;
+        pass = p;
+        email = e;
+    }
+
+    int getId() {
+        return id;
+    }
+
+    void setId(int i) {
+        id = i;
+    }
+
+    String getUname() {
+        return uname;
+    }
+
+    void setUname(String u) {
+        uname = u;
+    }
+
+    String getPass() {
+        return pass;
+    }
+
+    void setPass(String p) {
+        pass = p;
+    }
+
+    String getEmail() {
+        return email;
+    }
+
+    void setEmail(String e) {
+        email = e;
+    }
 }
